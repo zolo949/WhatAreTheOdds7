95d9d1b03a241599438a858f548ad231701377ab
Game Design <gamedesign@TCsiMac6.local>
2015-09-08 09:47:24 -0400

    Initial Commit

---
+++ BluetoothTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothTest
+//
+//  Created by Game Design on 9/8/15.
+//  Copyright (c) 2015 Game Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
